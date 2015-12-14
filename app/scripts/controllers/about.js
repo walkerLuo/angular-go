a837f1eb80eb3a21c384305bd55a62f389446ee0
walker <walker@bu2trip.com>
2015-12-14 13:58:47 +0800

    框架初始化

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularGoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularGoApp
+ */
+angular.module('angularGoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularGoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularGoApp
+ */
+angular.module('angularGoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
